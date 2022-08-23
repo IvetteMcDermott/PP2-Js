834ebc9263528fe435596b1748bb7e63b51fa362
IvetteMcDermott <ivettedemcdermott@gmail.com>
2022-08-23 13:43:22 +0000

    add final message conditional, cleanning Js, fixing bug caused by a typo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-/*gameOn();*/  
 });
-/** background audio button */
+/** background audio control button, play/pause */
     const music=document.getElementById("music");
     const audio=document.getElementById("audio");
        
@@ -25,7 +24,7 @@ let itemsInfo=[
     {img:2, opt1:"The shoes are black.", opt2:"The socks are pink.", opt3:"The pants are green.", correctAnswer:"The pants are green.",},
     {img:3, opt1:"The t-shirt is orange.", opt2:"The pants is orange.", opt3:"The pants are orange.", correctAnswer:"The pants are orange.",},
     {img:4, opt1:"The shorts are red.", opt2:"The pants are red.", opt3:"The pants are blue.", correctAnswer:"The pants are red.",},
-    {img:5, opt1:"The pants are yellow.", opt2:"The skirt is orange.", opt3:"The pants are blue.", correctAnswer:"the pants are yellow.",},
+    {img:5, opt1:"The pants are yellow.", opt2:"The skirt is orange.", opt3:"The pants are blue.", correctAnswer:"The pants are yellow.",},
     {img:6, opt1:"The shorts are blue.", opt2:"The shorts are red", opt3:"The shorts is blue.", correctAnswer:"The shorts are blue.",},
     {img:7, opt1:"The pants are green.", opt2:"The shorts are green.", opt3:"The shorts is red.", correctAnswer:"The shorts are green.",},
     {img:8, opt1:"The shorts are blue.", opt2:"The shorts are orange.", opt3:"The shorts are black.", correctAnswer:"The shorts are orange.",},
@@ -42,7 +41,7 @@ let itemsInfo=[
     {img:19, opt1:"The t-shirt is orange.", opt2:"The skirt is yellow.", opt3:"The t-shirt is red.", correctAnswer:"The t-shirt is red.",},
     {img:20, opt1:"The t-shirt is orange.", opt2:"The t-shirt is red.", opt3:"The t-shirt is yellow.", correctAnswer:"The t-shirt is yellow.",},
 ]
-
+/** click event for buttons */
 let buttons = document.getElementsByTagName("button")
 
     for (let clicked of buttons) {
@@ -52,61 +51,45 @@ let buttons = document.getElementsByTagName("button")
             start.style.visibility="hidden"; 
         }
         else if (this.getAttribute("data-type") === "getObject") {
-            
             getObject()
-
         } else if (this.getAttribute("data-type") === "checkSentence") {
-                    
            checkSent()
-
         } else if (this.getAttribute("data-type") === "help"){
             document.getElementById("helpInstructions").style.visibility="visible";
         } else if (this.getAttribute("data-type") === "closeInstructions"){
             document.getElementById("helpInstructions").style.visibility="hidden";
         } else if (this.getAttribute("data-type")=== "exit"){
             exit();
-        }
-        
+        }        
     })}
 
+/* functions that generate the ramdom number and pull the object accorder to */
 function ramdomNumb() {
     let numObj1 = Math.floor(Math.random() * itemsInfo.length)+1;
     return numObj1
-console.log(numObj1)}
+}
 
-/* game functions  */
 function ramdom() {
     let numObj1 = Math.floor(Math.random() * itemsInfo.length)+1;
     return numObj1
-console.log(numObj1)
-    /*let numObj=ramdomNumb()*/
-    let objToUse1=itemsInfo.find(({ img }) => img === numObj);
-return objToUse1
-console.log(objToUse1)
 }
-
-
+/** display the picture and the answer options */
 function getObject() {
-
     let objToUse=ramdom()
     let numObj=ramdomNumb()
     objToUse=itemsInfo.find(({ img }) => img === numObj);
-    console.log(objToUse)
-    console.log(numObj)
-    console.log("getObject triggered");
-    /*objToUse=itemsInfo.find(({ img }) => img === numObj);*/
+    
 /* assigning ramdom image */    
 let img=document.getElementById("picture");
 let imgFile= `${imgFolder + numObj}.png`
 img.src=imgFile
 
-/* generating answer options */
-
+/* pulling the varianles for answer options */
 let objOpt1= objToUse[0,'opt1'];
 let objOpt2= objToUse[0,'opt2'];
 let objOpt3= objToUse[0,'opt3'];
 let correctAnswer= objToUse[0,'correctAnswer'];
-
+/* assigning answer options */
 document.getElementById("labOpt1").innerHTML=objOpt1
 document.getElementById("labOpt2").innerHTML=objOpt2
 document.getElementById("labOpt3").innerHTML=objOpt3
@@ -114,26 +97,24 @@ document.getElementById("answerLabel").innerHTML=correctAnswer
 document.getElementById("opt1").value=objOpt1
 document.getElementById("opt2").value=objOpt2
 document.getElementById("opt3").value=objOpt3
-
+/** display the button that compare the answer selected with the correct one, when get a new picture */
 document.getElementById("checkSentence").style.visibility="visible";
-
+/** set radios to unchecked */
 document.getElementById("opt1").checked=false;
 document.getElementById("opt2").checked=false;
 document.getElementById("opt3").checked=false;
 }
-
+/** function that check the answer selected */
 function checkSent() {
-    console.log("checkSent triggered");
-
+    
 let correctAnswer=document.getElementById("answerLabel").innerHTML
-
 let displayPic=document.getElementById("result");
 let audio=document.getElementById("audioAnswer");
 
-/* query selector from Geekforgeeks*/ 
+/** pull the selected radio */ 
 let checkRadio = document.querySelector(
         'input[name="answer"]:checked');
-
+/** conditional that trigger actions accorder if result is right or wrong */
 if (checkRadio.value === correctAnswer){
     displayPic.src="assets/images/right-answer.gif"
     audioAnswer.src="assets/audio/woohoo.mp3"
@@ -152,35 +133,42 @@ if (checkRadio.value === correctAnswer){
     increaseTries();
 }
 }
-
+/** right answer count */
 function rightAnswerCount() {
-    console.log("rightAnswerCount triggered");
-
+    
 let rightAnswerCount=parseInt(document.getElementById("correctAnswer").innerText);
 document.getElementById("correctAnswer").innerText=++rightAnswerCount;
     console.log("rightAnswerCount", rightAnswerCount);
 }
-
+/** tries count */
 function increaseTries() {
-    console.log("increaseTries triggered");
-
+    
 let triesCount=parseInt(document.getElementById("tries").innerText);
 
 document.getElementById("tries").innerText=++triesCount;
 let gotRight=parseInt(document.getElementById("correctAnswer").innerHTML)
-    console.log("triesCount", triesCount);
+   /** final message about the result */ 
     if (triesCount>= 10) {
         document.getElementById("finalResultText").innerHTML=`You got ${gotRight} of ${triesCount}`;
-        /*alert(`you got ${gotRight} of ${triesCount}`)*/
+        let finalMessage=""
+        if (gotRight<=4){
+            finalMessage="Good try! Keep the practice."
+        } else if (gotRight=5 && gotRight<=8) {
+            finalMessage="Well done! Keep the good work."
+        } else {
+            finalMessage="Excellent job!"
+        }
+        document.getElementById("finalResultMessage").innerHTML=finalMessage;
+        
         document.getElementById("finalResult").style.visibility="visible";
-        setTimeout("location.reload(true);",4000);
+        setTimeout("location.reload(true);",6000);
         } 
 }
-
+/** exit button */
 function exit() {
     console.log("exit triggered");
     let exitYes = confirm("Do you want to exit the game?");
     if (exitYes)
         {alert("Try again soon!");
-            setTimeout("location.reload(true);",800);}
+            setTimeout("location.reload(true);",1200);}
     }
